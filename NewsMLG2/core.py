3a111c89ce7521b4d9042735cab978afc56daa64
benjkempf <161314342+benjkempf@users.noreply.github.com>
2025-05-21 21:51:22 +0200

    Update core.py
    added brackets in line 159-162

--- NewsMLG2/core.py
+++ NewsMLG2/core.py
@@ -156,8 +156,10 @@ class BaseObject():
             element_class_name = elemdefndict[name]['element_class']
             element_class = self.get_element_class(element_class_name)
             if isinstance(value, str):
-                if self._element_definitions['xml_type'] == 'xs:enumeration' and
-                    value not in self._element_definitions['enum_values']:
+                if (
+                    self._element_definitions['xml_type'] == 'xs:enumeration' and
+                    value not in self._element_definitions['enum_values']
+                ):
                     # validate that value is defined in the enum
                     raise AttributeError(
                             "Trying to assign a value not defined in enumeration"
